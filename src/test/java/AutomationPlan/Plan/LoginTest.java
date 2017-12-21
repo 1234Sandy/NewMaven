0807a108ad86de520d996b615c18c60e0ecc97a7
1234Sandy <sandydzee2017@gmail.com>
2017-12-20 22:39:23 -0700

    first commit

---
+++ src/test/java/AutomationPlan/Plan/LoginTest.java
@@ -0,0 +1,40 @@
+package AutomationPlan.Plan;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+public static WebDriver driver;
+	
+	@BeforeTest
+	public void setUp(){
+		
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+        driver =  new ChromeDriver();
+	}
+	
+	@Test
+	public void doLogin(){
+		 driver.get("https://www.google.com/");
+		 System.out.println("Hello");
+		 System.out.println("Welcome to hello world");
+		 System.out.println("Welcome to hello world");
+		 System.out.println("Welcome to hello world");
+		 System.out.println("Welcome to hello world");
+		 System.out.println("Welcome to hello world");
+		 System.out.println("Mybranch");
+		
+	}
+	
+	@AfterTest
+	public void tearDown(){
+	   driver.quit();		
+	}
+}
+
+
+
